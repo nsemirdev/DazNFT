a66f21033fd19681afbc52dcfa635eb2f4db40a0
nsemirdev <nsemirdev@gmail.com>
2022-12-27 23:43:12 +0300

    initial project

---
+++ DazNFT/Colors/Colors.swift
@@ -0,0 +1,62 @@
+//
+//  Colors.swift
+//  DazNFT
+//
+//  Created by Emir Alkal on 27.12.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+        
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+        
+        return nil
+    }
+}
+
+struct Colors {
+    struct Primary {
+        static let deepPurple = UIColor(hex: "#5F61F0FF")
+        static let dark = UIColor(hex: "#4C4EC0FF")
+        static let darker = UIColor(hex: "#393A90FF")
+        static let darkest = UIColor(hex: "#131330FF")
+        static let light = UIColor(hex: "#7f81f3FF")
+        static let lighter = UIColor(hex: "#9fa0f6FF")
+        static let lightest = UIColor(hex: "#dfdffcFF")
+    }
+    
+    struct Secondary {
+        static let dark = UIColor(hex: "#FFD14BFF")
+        static let darker = UIColor(hex: "#cca73cff")
+        static let darkest = UIColor(hex: "#66541eff")
+        static let light = UIColor(hex: "#ffda6fff")
+        static let lighter = UIColor(hex: "#ffe393ff")
+        static let lightest = UIColor(hex: "#fff6dbff")
+    }
+    
+    struct Text {
+        static let highEmp = UIColor(hex: "#454459ff")
+        static let mediumEmp = UIColor(hex: "#616161ff")
+        static let lowEmp = UIColor(hex: "#757575ff")
+    }
+}
---
+++ DazNFT/Fonts/Fonts.swift
@@ -0,0 +1,69 @@
+//
+//  Fonts.swift
+//  DazNFT
+//
+//  Created by Emir Alkal on 27.12.2022.
+//
+
+import UIKit
+
+struct Fonts {
+    struct Heading {
+        static let text1_1 = UIFont.systemFont(ofSize: 40, weight: .bold)
+        static let text1_2 = UIFont.systemFont(ofSize: 40, weight: .semibold)
+        static let text1_3 = UIFont.systemFont(ofSize: 40, weight: .medium)
+        static let text1_4 = UIFont.systemFont(ofSize: 40, weight: .regular)
+    
+        static let text2_1 = UIFont.systemFont(ofSize: 36, weight: .bold)
+        static let text2_2 = UIFont.systemFont(ofSize: 36, weight: .semibold)
+        static let text2_3 = UIFont.systemFont(ofSize: 36, weight: .medium)
+        static let text2_4 = UIFont.systemFont(ofSize: 36, weight: .regular)
+        
+        static let text3_1 = UIFont.systemFont(ofSize: 30, weight: .bold)
+        static let text3_2 = UIFont.systemFont(ofSize: 30, weight: .semibold)
+        static let text3_3 = UIFont.systemFont(ofSize: 30, weight: .medium)
+        static let text3_4 = UIFont.systemFont(ofSize: 30, weight: .regular)
+        
+        static let text4_1 = UIFont.systemFont(ofSize: 24, weight: .bold)
+        static let text4_2 = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        static let text4_3 = UIFont.systemFont(ofSize: 24, weight: .medium)
+        static let text4_4 = UIFont.systemFont(ofSize: 24, weight: .regular)
+        
+        static let text5_1 = UIFont.systemFont(ofSize: 18, weight: .bold)
+        static let text5_2 = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        static let text5_3 = UIFont.systemFont(ofSize: 18, weight: .medium)
+        static let text5_4 = UIFont.systemFont(ofSize: 18, weight: .regular)
+    }
+    struct General {
+        static let text1_1 = UIFont.systemFont(ofSize: 20, weight: .bold)
+        static let text1_2 = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        static let text1_3 = UIFont.systemFont(ofSize: 20, weight: .medium)
+        static let text1_4 = UIFont.systemFont(ofSize: 20, weight: .regular)
+        static let text1_5 = UIFont.systemFont(ofSize: 20, weight: .light)
+    
+        static let text2_1 = UIFont.systemFont(ofSize: 16, weight: .bold)
+        static let text2_2 = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        static let text2_3 = UIFont.systemFont(ofSize: 16, weight: .medium)
+        static let text2_4 = UIFont.systemFont(ofSize: 16, weight: .regular)
+        static let text2_5 = UIFont.systemFont(ofSize: 16, weight: .light)
+
+        static let text3_1 = UIFont.systemFont(ofSize: 14, weight: .bold)
+        static let text3_2 = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        static let text3_3 = UIFont.systemFont(ofSize: 14, weight: .medium)
+        static let text3_4 = UIFont.systemFont(ofSize: 14, weight: .regular)
+        static let text3_5 = UIFont.systemFont(ofSize: 14, weight: .light)
+
+        static let text4_1 = UIFont.systemFont(ofSize: 12, weight: .bold)
+        static let text4_2 = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        static let text4_3 = UIFont.systemFont(ofSize: 12, weight: .medium)
+        static let text4_4 = UIFont.systemFont(ofSize: 12, weight: .regular)
+        static let text4_5 = UIFont.systemFont(ofSize: 12, weight: .light)
+        
+        static let text5_1 = UIFont.systemFont(ofSize: 10, weight: .bold)
+        static let text5_2 = UIFont.systemFont(ofSize: 10, weight: .semibold)
+        static let text5_3 = UIFont.systemFont(ofSize: 10, weight: .medium)
+        static let text5_4 = UIFont.systemFont(ofSize: 10, weight: .regular)
+        static let text5_5 = UIFont.systemFont(ofSize: 10, weight: .light)
+    }
+}
+
